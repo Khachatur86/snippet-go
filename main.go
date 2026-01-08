95155f0c57b4abc50678f12b3001321c0845746e
Khachatur Sarkisyan <sarkhv@gmail.com>
2026-01-09 00:23:27 +0300

    Update main.go: add HTTP server with home handler

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
-func main() {
-    fmt.Println("Hello world!!!")
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
