e53aa6819348e3b7f0f97836d1f1f11782a420a4
Khachatur Sarkisyan <sarkhv@gmail.com>
2026-01-09 23:18:06 +0300

    Simplify root route pattern from /{$} to /

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 
